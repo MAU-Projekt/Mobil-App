4791a234cc0025b8ab118d8e4947a0ed8ffb8101
kroook <fabbekrook@gmail.com>
2021-03-30 09:30:53 +0200

    test2

--- app/src/main/java/com/example/projektkruka/MainActivity.java
+++ app/src/main/java/com/example/projektkruka/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+    private TextView outputText;
+    private int lightvalue = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        outputText =(TextView) findViewById(R.id.outputTextView);
+        outputText.setText(String.valueOf(lightvalue));
 
         Button btnTand = findViewById(R.id.btnTand);
         Button btnSlack = findViewById(R.id.btnSlack);
@@ -17,15 +24,18 @@ class MainActivity extends AppCompatActivity {
         btnTand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                System.out.println("Tänd");
 
             }
         });
         btnSlack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                System.out.println("Släck");
 
             }
         });
+
     }
 
 }
