6b9f0dd30bd7027a302401cc43856cf1847121fb
kroook <fabbekrook@gmail.com>
2021-03-30 17:25:24 +0200

    2

--- AndroidApp/app/src/main/java/com/example/projektkruka/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/projektkruka/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.projektkruka;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
     private TextView outputText;
@@ -15,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        outputText =(TextView) findViewById(R.id.outputTextView);
+        outputText = (TextView) findViewById(R.id.outputTextView);
         outputText.setText(String.valueOf(lightvalue));
 
         Button btnTand = findViewById(R.id.btnTand);
@@ -38,4 +42,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-}
+    public void ChangeFragment(View view) {
+        Fragment fragment;
+        if (view == findViewById(R.id.btnkruka1)) {
+            fragment = new Fragment1();
+            FragmentManager fm = getSupportFragmentManager();
+            androidx.fragment.app.FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.Fragment, fragment);
+            ft.commit();
+        }
+        if (view == findViewById(R.id.btnkruka2)) {
+            fragment = new Fragment2();
+            FragmentManager fm = getSupportFragmentManager();
+            androidx.fragment.app.FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.Fragment, fragment);
+            ft.commit();
+        }
+        }
+
+
+    }
