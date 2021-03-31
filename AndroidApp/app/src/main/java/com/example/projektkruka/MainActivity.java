ccf17612ae788a36874b362c018e8909e9505638
kroook <fabbekrook@gmail.com>
2021-03-31 17:03:16 +0200

    2

--- AndroidApp/app/src/main/java/com/example/projektkruka/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/projektkruka/MainActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.app.FragmentTransaction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     private TextView outputText;
-    private int lightvalue = 10;
+    private String lightvalue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnTand = findViewById(R.id.btnTand);
         Button btnSlack = findViewById(R.id.btnSlack);
+        Button btnUpdate = findViewById(R.id.btnUpdate);
 
         btnTand.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,8 +49,74 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doStuff();
+            }
+        });
+    }
+
+    private void doStuff(){
+        new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                lightvalue = getSiteString("https://tools.learningcontainer.com/sample-json-file.json");
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        outputText.setText(lightvalue);
+
+                    }
+                });
+
+            }
+        }).start();
+    }
+
+    private String getSiteString(String site){
+        HttpURLConnection connection = null;
+        URL url;
+        BufferedReader reader = null;
+        String output = "";
+
+            try {
+                url = new URL(site);
+                connection =(HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+                output = buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally{
+                if(connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if(reader != null){
+                        reader.close();}
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return output;
+
 
     }
+
 
     public void ChangeFragment(View view) {
         Fragment fragment;
@@ -59,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
             ft.commit();
         }
         }
+
 
 
     }
